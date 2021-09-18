8c9516780a2c04741af22e936971f9a4febb5bfe
ambrosemcduffy <ambrosemcduffy@gmail.com>
2021-09-18 13:28:57 -0400

    updating documentation of union intersection

--- Union_Intersection.py
+++ Union_Intersection.py
@@ -8,7 +8,7 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.n_entries = 0
-    def append(self, value):
+    def append(self,value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -32,6 +32,12 @@ class LinkedList:
 
 
 def linkedlistToArray(linkedlist):
+    """ Appends a linked list to sorted array
+    Args:
+        linked_list (class): node class with values
+    Returns:
+        list: sorted array of values
+    """
     elem_l = []
     node = linkedlist.head
     while node:
@@ -40,14 +46,24 @@ def linkedlistToArray(linkedlist):
     return sorted(set(elem_l))
 
 def intersect(linked_list_1, linked_list_2):
-    elem_set1 = linkedlistToArray(linked_list_1)
-    elem_set2 = linkedlistToArray(linked_list_2)
-    ll = LinkedList()
-    for i in elem_set1:
-        for j in elem_set2:
+    """ Appends intersecting values to new linked list node
+    Args:
+        linked_list_1 (class): node class with values
+        linked_list_2 (class): node class with values
+    Returns:
+        class: node class with intersecting values
+    """
+    
+    array_1 = linkedlistToArray(linked_list_1)
+    array_1 = linkedlistToArray(linked_list_2)
+    intersecting_linked_list = LinkedList()
+
+    for i in array_1:
+        for j in array_1:
             if i == j:
-                ll.append(i)
-    return ll
+                intersecting_linked_list.append(i)
+    
+    return intersecting_linked_list
 
 
 def union(linked_list_1, linked_list_2):
@@ -60,17 +76,22 @@ def union(linked_list_1, linked_list_2):
     return ll
 
 
-linked_list_1 = LinkedList()
-linked_list_2 = LinkedList()
-
 element_1 = [3,2,4,35,6,65,6,4,3,21]
 element_2 = [6,32,4,9,6,1,11,21,1]
 
-for element in element_1:
-    linked_list_1.append(element)
+def append_to_linked_list(list_):
+    linked_list = LinkedList()
+    for element in list_:
+        linked_list.append(element)
+    return linked_list
 
-for element in element_2:
-    linked_list_2.append(element)
+
+linked_list_1 = append_to_linked_list(element_1)
+linked_list_2 = append_to_linked_list(element_2)
+
+intersect_ll = intersect(linked_list_1, linked_list_2)
+union_ll = union(linked_list_1, linked_list_2)
+
 
 
 
@@ -79,10 +100,7 @@ def printOutLinkedList(linked_list):
     print("\n")
     while node:
         print("-{}->".format(node.value), end='')
-        node = node.next
-
-intersect_ll = intersect(linked_list_1, linked_list_2)
-union_ll = union(linked_list_1, linked_list_2)
+        node = node.next\
 
 printOutLinkedList(intersect_ll)
 printOutLinkedList(union_ll)
